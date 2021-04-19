530abe8e26876977d336d3b26ed5f1ac199d491b
markdhooper <markushooper@gmail.com>
2021-04-19 16:35:21 -0300

    started adding other steps. hosts is working

--- src/fakeroot/usr/share/cockpit/ceph-deploy/ceph-deploy.js
+++ src/fakeroot/usr/share/cockpit/ceph-deploy/ceph-deploy.js
@@ -18,23 +18,25 @@
 let g_core_params = null;
 
 
-function show_snackbar_msg(msg_label,msg_content,msg_color) {
-	var snackbar = document.getElementById("snackbar");
-	snackbar.innerHTML = msg_label + msg_content;
-	snackbar.style.backgroundColor = msg_color;
-	snackbar.className = "show";
-	setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+function show_snackbar_msg(msg_label,msg_content,msg_color,id) {
+	var snackbar = document.getElementById(id);
+	if(snackbar != null){
+		snackbar.innerHTML = msg_label + msg_content;
+		snackbar.style.backgroundColor = msg_color;
+		snackbar.className = "show";
+		setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+	}
   } 
 
 function add_host_request(){
 	hostname = document.getElementById("new-hostname-field").value;
-	monitor_interface = "";
-	ip = "";
+	monitor_interface = document.getElementById("new-interface-field").value;
+	ip = document.getElementById("new-ip-field").value;
 	if(hostname != null && hostname != ""){
 		host_request_json = {[hostname]:{"hostname":""}};
 		host_request_json[hostname]["hostname"] = hostname;
-		if(ip != null) host_request_json[hostname]["ip"] = ip;
-		if(monitor_interface != null) host_request_json[hostname]["monitor_interface"] = monitor_interface;
+		host_request_json[hostname]["ip"] = (ip != null) ? ip:"";
+		host_request_json[hostname]["monitor_interface"] = (monitor_interface != null) ? monitor_interface:"";
 		var spawn_args = ["/usr/share/cockpit/ceph-deploy/helper_scripts/core_params","-h",JSON.stringify(host_request_json),"-w"];
 		var result_json = null;
 		var proc = cockpit.spawn(spawn_args, {superuser: "require"});
@@ -51,15 +53,16 @@ function add_host_request(){
 			}
 			if (result_json.hasOwnProperty("success_msg")){
 				msg_color = "#20a030";
-				msg_label = "Add Host Successful: ";
-				msg_content = result_json.success_msg;
+				msg_label = "Add Host: ";
+				msg_content = "Host Added Succcessfully.";
 			}else{
 				msg_color = "#bd3030";
 				msg_label = "Error:";
 				msg_content = "Unexpected return value.";
 			}
 			hide_modal_dialog("add-host-modal");
-			show_snackbar_msg(msg_label,msg_content,msg_color);
+			get_param_file_content();
+			show_snackbar_msg(msg_label,msg_content,msg_color,"add-host-snackbar");
 		});
 		proc.fail(function(ex, data) {
 			document.getElementById("add-host-result-msg").style.display = "block";
@@ -85,13 +88,23 @@ function add_host_request(){
 }
 
 function add_host(){
+	document.getElementById("add-host-modal-title").innerText = "Add New Host";
+	
+	let hostname_field = document.getElementById("new-hostname-field");
+	let ip_field = document.getElementById("new-ip-field");
+	let interface_field = document.getElementById("new-interface-field");
+	hostname_field.value = "";
+	ip_field.value = "";
+	interface_field.value = "";
+
     show_modal_dialog("add-host-modal");
-    document.getElementById("new-hostname-field").addEventListener("input",function(){
+	hostname_field.addEventListener("input",function(){
 		check_name_field("new-hostname-field","new-hostname-field-feedback","continue-add-host","hostname",true)});
-	document.getElementById("new-ip-field").addEventListener("input",function(){
+	ip_field.addEventListener("input",function(){
 		check_ip_field("new-ip-field","new-ip-field-feedback","continue-add-host","ip",false)});
-	document.getElementById("new-interface-field").addEventListener("input",function(){
+	interface_field.addEventListener("input",function(){
 		check_name_field("new-interface-field","new-interface-field-feedback","continue-add-host","monitor_interface",false)});
+
     document.getElementById("close-add-host").addEventListener("click",function(){ hide_modal_dialog("add-host-modal"); });
     document.getElementById("cancel-add-host").addEventListener("click",function(){ hide_modal_dialog("add-host-modal"); });
     document.getElementById("continue-add-host").addEventListener("click",add_host_request);
@@ -190,30 +203,98 @@ function get_param_file_content(){
 			msg_content = "Unexpected return value.";
 		}
 		hide_modal_dialog("add-host-modal");
-		show_snackbar_msg(msg_label,msg_content,msg_color);
+		show_snackbar_msg(msg_label,msg_content,msg_color,"snackbar");
 	});
 	proc.fail(function(ex, data) {
 		var msg_color = "#bd3030";
 		var msg_label = "Error:";
 		var msg_content = "Unable to load parameter file.";
-		show_snackbar_msg(msg_label,msg_content,msg_color);
+		show_snackbar_msg(msg_label,msg_content,msg_color,"snackbar");
 	});
 }
 
+function remove_host(hostname){
+	host_request_json = {[hostname]:{"hostname":{}}};
+		host_request_json[hostname]["hostname"] = hostname;
+		var spawn_args = ["/usr/share/cockpit/ceph-deploy/helper_scripts/core_params","-h",JSON.stringify(host_request_json),"-x"];
+		var result_json = null;
+		var proc = cockpit.spawn(spawn_args, {superuser: "require"});
+		proc.done(function(data) {
+			var msg_label = "";
+			var msg_content = "";
+			var msg_color = "";
+			console.log("Result: ",data);
+			try {
+				result_json = JSON.parse(data);
+			} catch (e) {
+				msg_color = "#bd3030";
+				msg_label = "Error:";
+				msg_content = "Unexpected return value.";
+			}
+			if (result_json.hasOwnProperty("success_msg")){
+				msg_color = "#20a030";
+				msg_label = "Add Host: ";
+				msg_content = result_json.success_msg;
+			}else{
+				msg_color = "#bd3030";
+				msg_label = "Error:";
+				msg_content = "Unexpected return value.";
+			}
+			get_param_file_content();
+			show_snackbar_msg(msg_label,msg_content,msg_color,"add-host-snackbar");
+		});
+		proc.fail(function(ex, data) {
+			var msg_label = "Error: ";
+			var msg_content = ""
+			var msg_color = "#bd3030";
+			try {
+				result_json = JSON.parse(data);
+			} catch (e) {
+				msg_content = "Unable to remove host";
+			}
+			if (result_json.hasOwnProperty("error_msg")){
+				msg_content = result_json.error_msg;
+			}else{
+				msg_content = "Unable to remove host";
+			}
+			show_snackbar_msg(msg_label,msg_content,msg_color,"add-host-snackbar");
+		});
+}
+
+function edit_host(hostname,ip,monitor_interface){
+	document.getElementById("add-host-modal-title").innerText = "Edit Host";
+	
+	let hostname_field = document.getElementById("new-hostname-field");
+	let ip_field = document.getElementById("new-ip-field");
+	let interface_field = document.getElementById("new-interface-field");
+	
+	hostname_field.value = hostname;
+	ip_field.value = ip;
+	interface_field.value = monitor_interface;
+
+	show_modal_dialog("add-host-modal");
+	hostname_field.addEventListener("input",function(){
+		check_name_field("new-hostname-field","new-hostname-field-feedback","continue-add-host","hostname",true)});
+	ip_field.addEventListener("input",function(){
+		check_ip_field("new-ip-field","new-ip-field-feedback","continue-add-host","ip",false)});
+	interface_field.addEventListener("input",function(){
+		check_name_field("new-interface-field","new-interface-field-feedback","continue-add-host","monitor_interface",false)});
+
+	document.getElementById("close-add-host").addEventListener("click",function(){ hide_modal_dialog("add-host-modal"); });
+	document.getElementById("cancel-add-host").addEventListener("click",function(){ hide_modal_dialog("add-host-modal"); });
+	document.getElementById("continue-add-host").addEventListener("click",add_host_request);
+}
+
 function update_host_info(hosts_json){
-	var hostname = "";
-	var ip = "";
-	var monitor_interface = "";
-	var host_list = document.getElementById("cd-host-list");
-	if(Object.keys(hosts_json).length > 0){
-		while (host_list.hasChildNodes()) {  
-			host_list.removeChild(host_list.firstChild);
-		}
-		
-		for (var key of Object.keys(hosts_json)) {
-			hostname = hosts_json[key]["hostname"];
-			ip = hosts_json[key]["ip"];
-			monitor_interface = hosts_json[key]["monitor_interface"];
+	let host_list = document.getElementById("cd-host-list");
+	while (host_list.hasChildNodes()) {  
+		host_list.removeChild(host_list.firstChild);
+	}
+	if(Object.keys(hosts_json).length > 0){	
+		for (let key of Object.keys(hosts_json)) {
+			let hostname = hosts_json[key]["hostname"];
+			let ip = hosts_json[key]["ip"];
+			let monitor_interface = hosts_json[key]["monitor_interface"];
 			
 			var new_host_entry = document.createElement("div");
 			new_host_entry.classList.add("cd-host-list-entry");
@@ -231,12 +312,18 @@ function update_host_info(hosts_json){
 			host_entry_monitor_interface.innerText = monitor_interface;
 
 			var host_entry_edit_icon = document.createElement("div");
-			host_entry_edit_icon.classList.add("cd-host-list-entry-icon");
-			host_entry_edit_icon.innerHTML = "&#9881;";
+			host_entry_edit_icon.classList.add("cd-host-list-entry-icon","fa","fa-gear");
+
+			host_entry_edit_icon.addEventListener("click", function(){
+				let hns = hostname.valueOf();
+				let ips = ip.valueOf();
+				let mis = monitor_interface.valueOf();
+				edit_host(hns,ips,mis);
+			});
 
 			var host_entry_delete_icon = document.createElement("div");
-			host_entry_delete_icon.classList.add("cd-host-list-entry-icon");
-			host_entry_delete_icon.innerHTML = "&times";
+			host_entry_delete_icon.classList.add("cd-host-list-entry-icon-del","fa","fa-times");
+			host_entry_delete_icon.addEventListener("click", function(){let arg = hostname.valueOf();remove_host(arg)});
 
 			new_host_entry.appendChild(host_entry_hostname);
 			new_host_entry.appendChild(host_entry_ip);
@@ -247,8 +334,24 @@ function update_host_info(hosts_json){
 			host_list.appendChild(new_host_entry);
 		}
 	}else{
-
+		var placeholder = document.createElement("div");
+		placeholder.classList.add("cd-host-list-entry-placeholder");
+		placeholder.id = "cd-host-placeholder";
+		placeholder.innerText = "No hosts have been configured. Click the \"+New Host\" button to add a new host.";
+		host_list.appendChild(placeholder);
 	}
+}
+
+function hide_step_content(step){
+	let id_string = "step-" + step.toString();
+	let step_content = document.getElementById(id_string);
+	if(step_content != null){step_content.classList.add("hidden");}
+}
+
+function show_step_content(step){
+	let id_string = "step-" + step.toString();
+	let step_content = document.getElementById(id_string);
+	if(step_content != null){step_content.classList.remove("hidden");}
 }
 
 function main()
@@ -260,8 +363,32 @@ function main()
 			if(root_check.allowed){
 				//user is an administrator, start the module as normal
                 //setup on-click listeners for buttons as required.
+				var current_step = Number(localStorage.getItem("current_step")??"0");
+				if(current_step === 0){ document.getElementById("prev-step-btn").disabled = true; }
+
+				show_step_content(current_step);
 				get_param_file_content();
+				
 				document.getElementById("new-host-btn").addEventListener("click",add_host);
+
+				document.getElementById("next-step-btn").addEventListener("click",() => {
+					var next_step = Number(localStorage.getItem("current_step")??"0") + 1;
+					var current_step = next_step -1;
+					hide_step_content(current_step);
+					show_step_content(next_step);
+					document.getElementById("prev-step-btn").removeAttribute("disabled");
+					localStorage.setItem("current_step",next_step.toString());
+				});
+				
+				document.getElementById("prev-step-btn").addEventListener("click",() => {
+					var prev_step = Number(localStorage.getItem("current_step")??"0") == 0 ? 0 : Number(localStorage.getItem("current_step")??"0") - 1;
+					var current_step = prev_step +1;
+					hide_step_content(current_step);
+					show_step_content(prev_step);
+					if(prev_step !== 0){document.getElementById("prev-step-btn").removeAttribute("disabled");}
+					else{document.getElementById("prev-step-btn").disabled = true;}
+					localStorage.setItem("current_step",prev_step.toString());
+				});
 			}else{
 				//user is not an administrator, inform them of this by
 				//displaying a message on each tab page. 
@@ -273,7 +400,7 @@ function main()
 				page_content.appendChild(user_msg);
 			}
 	 	}
-	)
+	);
 }
 
 main();
